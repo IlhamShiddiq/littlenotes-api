0eca2050c8f44e1c8d9744379ee21644d5167b95
IlhamShiddiq <shdqillham123@gmail.com>
2024-02-10 16:21:50 +0700

    fix: database connection config

--- config/db.js
+++ config/db.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const { CONFIGS } = require('./configs');
 
 const connect = async () => {
-    const dbConfig = CONFIGS.db;
-    await mongoose.connect(`mongodb://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}`, {
-        dbName: dbConfig.name
+    const { username, password, host, port, name } = CONFIGS.db;
+    let urlConnection
+
+    if (username && password) {
+        urlConnection = `mongodb://${username}:${password}@${host}:${port}`;
+    } else {
+        urlConnection = `mongodb://${host}:${port}`;
+    }
+
+    await mongoose.connect(urlConnection, {
+        dbName: name
     }).then(() => console.log('Database Connected!')).catch(err => console.log(err));
 }
 
